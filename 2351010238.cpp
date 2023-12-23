84ee9eb29811031c18f2f5e1e46fc2f70a5b14c0
lekhactung <80756257+lekhactung@users.noreply.github.com>
2023-12-23 11:33:59 +0700

    Update 2351010238.cpp

--- 2351010238.cpp
+++ 2351010238.cpp
@@ -28,8 +28,14 @@ int main(){
 		{
 			cout << "chuc nang tinh tong s: \n ";
 			int n, sum = 0;
-			cout << "nhap n: ";
-			cin >> n;
+			do 
+			{
+				cout << "nhap n: ";
+				cin >> n;
+				if (n <= 0) {
+					cout << " vui long nhap lai n: ";
+				}
+			} while (n <= 0);
 			for (int i = 0; i <= n; i++)
 			{
 				sum += i;
